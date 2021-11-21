0a7aa29832fdcc70bf415ef4d0f43bb0e0f31d06
Stephen <stephen.c.weiss@gmail.com>
2021-11-21 13:44:26 -0600

    🐛 Fix TSC and start scripts

---
+++ src/server/app.ts
@@ -0,0 +1,24 @@
+const express = require('express')
+
+export const app = express()
+const todoRouter = express.Router()
+
+type Todo = {
+  id: number
+  description: string
+  done?: true
+}
+const db: Todo[] = []
+
+todoRouter.route('/').get((req: any, res: any) => res.send(db))
+todoRouter
+  .route('/:id')
+  .get((req: any, res: any) => new Error('Not yet implemented'))
+  .post((req: any, res: any) => new Error('Not yet implemented'))
+  .put((req: any, res: any) => new Error('Not yet implemented'))
+  .patch((req: any, res: any) => new Error('Not yet implemented'))
+  .delete((req: any, res: any) => new Error('Not yet implemented'))
+
+app.use('/todos', todoRouter)
+
+app.get('*', (_req: any, res: any) => res.status(404).send())
