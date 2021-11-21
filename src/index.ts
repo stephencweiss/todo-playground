dc41df97bbf8d6b83026b3302f4dfe57abb95276
Stephen <stephen.c.weiss@gmail.com>
2021-11-21 13:50:53 -0600

    ✨ Make PORT dynamic

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { app } from './server/app'
 
-app.listen(3000, () => {
-  console.log('running on port 3000')
+const PORT = process.env.PORT ?? 3000
+app.listen(PORT, () => {
+  console.log(`running on port ${PORT}`)
 })
