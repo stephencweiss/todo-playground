ad60f41656bcf6b85f8e43e13a4fd91d51e851e4
Stephen <stephen.c.weiss@gmail.com>
2021-11-21 10:51:26 -0600

    ✨ Add basic tests
    Using Jest and Supertest, this commit includes a basic test for
    the API.

---
+++ server/app.test.ts
@@ -0,0 +1,16 @@
+import request from "supertest";
+import { app } from "./app";
+
+describe("Server", () => {
+  test("/GET todos", async () => {
+    await request(app)
+      .get("/todos")
+      .expect(200)
+      .then((response) => {
+        expect(response.body).toEqual([]);
+      });
+  });
+  test("/GET unknown path returns 404", async () => {
+    await request(app).get("/unknown-path").expect(404);
+  });
+});
---
+++ server/app.ts
@@ -0,0 +1,25 @@
+import express from "express";
+
+export const app = express();
+const todoRouter = express.Router();
+
+type Todo = {
+  id: number;
+  description: string;
+  done?: true;
+};
+const db: Todo[] = [];
+
+todoRouter.route("/").get((req, res) => res.send(db));
+todoRouter
+  .route("/:id")
+  .get((req, res) => new Error("Not yet implemented"))
+  .post((req, res) => new Error("Not yet implemented"))
+  .put((req, res) => new Error("Not yet implemented"))
+  .patch((req, res) => new Error("Not yet implemented"))
+  .delete((req, res) => new Error("Not yet implemented"));
+
+app.use("/todos", todoRouter);
+
+app.get("*", (_, res) => res.status(404).send());
+
