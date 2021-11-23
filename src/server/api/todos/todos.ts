f510f601e5245ae02e9dbef09dacd259cd17d70b
Stephen <stephen.c.weiss@gmail.com>
2021-11-22 19:25:42 -0600

    ✨ Add GET for /todos/:id

--- src/server/api/todos/todos.ts
+++ src/server/api/todos/todos.ts
@@ -5,14 +5,30 @@ export const todoRouter = require('express').Router()
 todoRouter
   .route('/')
   .get((req: Request, res: Response<Todo[]>) => res.send(db))
-  .post((req: Request<{},{}, BaseTodo>, res: Response<Todo>) => {
+  .post((req: Request<{}, {}, BaseTodo>, res: Response<Todo>) => {
     const insertedTodo = insertTodo(req.body)
     return res.status(201).send(insertedTodo)
   })
+
 todoRouter
   .route('/:id')
-  .get((req: Request, res: Response) => new Error('Not yet implemented'))
+  .get((req: Request<{ id: string }>, res: Response) => {
+    try {
+      const { id } = req.params
+      const found = db.find((todo) => todo.id === Number(id))
+      if (!found) throw new Error(`No TODO with id ${id}`)
+      res.send(found)
+    } catch (e: unknown) {
+      if (hasMessage(e)) {
+        res.status(404).send({ error: e.message })
+      }
+      res.status(404)
+    }
+  })
   .put((req: Request, res: Response) => new Error('Not yet implemented'))
   .patch((req: Request, res: Response) => new Error('Not yet implemented'))
   .delete((req: Request, res: Response) => new Error('Not yet implemented'))
 
+function hasMessage(arg: unknown): arg is { message: any } {
+  return typeof arg === 'object' && arg !== null && 'message' in arg
+}
--- src/server/app.test.ts
+++ src/server/app.test.ts
@@ -2,7 +2,7 @@ const request = require('supertest')
 const { app } = require('./app')
 
 describe('Server', () => {
-  test('/GET todos', async () => {
+  test('GET /todos', async () => {
     await request(app)
       .get('/api/todos')
       .expect(200)
@@ -10,7 +10,16 @@ describe('Server', () => {
         expect(response.body).toEqual([])
       })
   })
-  test('/POST todos', async () => {
+  test('GET /todos/:id', async () => {
+    const id = 1
+    await request(app)
+      .get(`/api/todos/${id}`)
+      .expect(404)
+      .then((response: any) => {
+        expect(response.error.text).toContain(`No TODO with id ${id}`)
+      })
+  })
+  test('POST /todos', async () => {
     const test = { description: 'Test' }
     await request(app)
       .post('/api/todos')
