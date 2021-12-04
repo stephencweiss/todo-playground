cc906e97cd26f53f35fc52139de6281ee0e94c37
Stephen <stephen.c.weiss@gmail.com>
2021-12-03 20:34:46 -0600

    ✨ Add DB connection strings
    This allows me to connect to a db in a convenient fashion, though
    I do need to adjust this a bit for testing. That's the next step.
    
    The changes to tsconfig allow for modules.

---
+++ src/db/connection.ts
@@ -0,0 +1,39 @@
+import { MongoClient } from 'mongodb'
+import { config } from '../config'
+
+const CONNECTION_STRING = `mongodb+srv://${config.dbUser}:${config.dbPassword}@sandbox.tv0tb.mongodb.net/todo-playground?retryWrites=true&w=majority`
+
+async function connect() {
+  const client = new MongoClient(CONNECTION_STRING, {
+        maxPoolSize: 10,
+        minPoolSize: 1
+  })
+
+  await client.connect()
+  return client
+}
+
+export const pool = await connect()
+export const _db = await pool.db('todo-playground')
+
+// export async function listDatabases(client: any) {
+//   const databasesList = await client.db().admin().listDatabases()
+
+//   console.log('Databases:')
+//   databasesList.databases.forEach((db: any) => console.log(` - ${db.name}`))
+// }
+
+export async function useDatabase(client: any, dbName: string) {
+  const used = await client.db(dbName)
+  console.log({ used })
+  return used
+}
+
+// export async function listCollections(db: any) {
+//   const collectionsList = await db.listCollections()
+//   console.log('Collections:')
+//   await collectionsList.forEach((c: any) => console.log({ c }))
+//   await collectionsList.rewind()
+//   const arr = await collectionsList.toArray()
+//   console.log({ arr })
+// }
