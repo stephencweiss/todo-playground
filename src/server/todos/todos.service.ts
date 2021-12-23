0d64343bc01bc050972cd016dcd34cf1dca09f9a
Stephen <stephen.c.weiss@gmail.com>
2021-12-22 23:46:03 -0600

    💅 use implicit returns; userDal replaces old dal

--- src/server/todos/todos.service.ts
+++ src/server/todos/todos.service.ts
@@ -5,24 +5,16 @@ import { User } from '../users'
 
 export const fetchTodos = async (
   query?: FilterQuery<Todo>,
-): Promise<any | Todo[]> => {
-  return await todoDal.find(query)
-}
+): Promise<any | Todo[]> => await todoDal.find(query)
 
-export const fetchTodoById = async (id: string): Promise<Todo | null> => {
-  return await todoDal.findById(id)
-}
+export const fetchTodoById = async (id: string): Promise<Todo | null> =>
+  await todoDal.findById(id)
 
 export const insertTodo = async (
   todo: CreateTodoInput & { createdBy?: User },
-): Promise<Todo> => {
-  return await todoDal.save(todo)
-}
+): Promise<Todo> => await todoDal.save(todo)
 
-export const updateTodo = async (id: string, updates: Partial<Todo>) => {
-  return await todoDal.update(id, updates)
-}
+export const updateTodo = async (id: string, updates: Partial<Todo>) =>
+  await todoDal.update(id, updates)
 
-export const removeTodo = async (id: string) => {
-  return await todoDal.deleteById(id)
-}
+export const removeTodo = async (id: string) => await todoDal.deleteById(id)
--- src/server/users/user.service.ts
+++ src/server/users/user.service.ts
@@ -1,22 +1,16 @@
 import { FilterQuery } from 'mongoose'
 import { User } from './users.model'
-import * as dal from './user.dal'
+import { userDal } from './user.dal'
 
-export const updateUser = async (id: string, updates: Partial<User>) => {
+export const updateUser = async (id: string, updates: Partial<User>) =>
   // TODO: should probably check permissions to make sure you're updating your own user and/or you have permission
-  return await dal.updateUser(id, updates)
-}
+  await userDal.update(id, updates)
 
 export const fetchUsers = async (
   query?: FilterQuery<User>,
-): Promise<any | User[]> => {
-  return await dal.findUser(query)
-}
+): Promise<any | User[]> => await userDal.find(query)
 
-export const fetchUserById = async (id: string): Promise<User | null> => {
-  return await dal.findUserById(id)
-}
+export const fetchUserById = async (id: string): Promise<User | null> =>
+  await userDal.findById(id)
 
-export const removeUser = async (id: string) => {
-  return await dal.removeUser(id)
-}
+export const removeUser = async (id: string) => await userDal.deleteById(id)
