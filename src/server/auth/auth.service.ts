01af23dedb19b57f056339de40fd7acde9dd96de
Stephen <stephen.c.weiss@gmail.com>
2021-12-22 23:51:52 -0600

    ✨ use UserDal in auth service
    Instead of making direct calls, the auth service now uses the same
    abstraction for accessing users as everything else.

--- src/server/auth/auth.service.ts
+++ src/server/auth/auth.service.ts
@@ -1,11 +1,11 @@
 import { UserDTO } from '../users/users.model'
-import * as dal from '../users/user.dal'
+import { userDal } from '../users/user.dal'
 import { hasMessage } from '../../utils'
 import { generateToken, verifyPassword } from '../../utils/auth'
 
 export const signup = async (user: UserDTO): Promise<string> => {
   try {
-    const { _id: id, username, email } = await dal.saveUser(user)
+    const { _id: id, username, email } = await userDal.save(user)
     return generateToken({ id, email, username })
   } catch (error) {
     // I suspect this will error if we violate constraints on the db (i.e., we try to create a duplicate user)
@@ -13,19 +13,13 @@ export const signup = async (user: UserDTO): Promise<string> => {
     return 'fail'
   }
 }
-// I don't like anything about this. Need ot figure out a better division of labor between router/service/dal
 
 export const signin = async (
   email: string,
   password: string,
 ): Promise<string> => {
   try {
-    // todo - figure out what i'm doing wrong with the types that i can't do .select / .lean, etc.
-    const user = await dal.findOneUser({ email })
-    // const user = ((await dal.findUser({ email })) as any)
-    //   .select('email password')
-    //   .exec()
-    console.log({ user })
+    const user = await userDal.findOne({ email })
 
     if (!user) {
       throw new Error('401')
@@ -36,9 +30,6 @@ export const signin = async (
     if (!match) {
       throw new Error('401')
     }
-    // if (!match) {
-    //   return res.status(401).send(invalid)
-    // }
     const { _id: id, username } = user
     return generateToken({ id, email, username })
   } catch (e) {
@@ -47,13 +38,4 @@ export const signin = async (
     }
     throw new Error('500')
   }
-
-  // try {
-  //   const user = await dal.findUser({ username })
-  //   // the user _should_ have an instance method of `validPassword`
-  //   user.map((u) => console.log({ u }))
-  //   return 'success!'
-  // } catch (error) {
-  //   return 'failed!'
-  // }
 }
